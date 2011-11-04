7d7607eeb4b9cf2eee2d351058d62d362be58773
Lance Hepler <nlhepler@gmail.com>
2011-11-03 19:04:45 -0700

    don't forget _preprocessing.py

--- src/hy454/__init__.py
+++ src/hy454/__init__.py
@@ -7,6 +7,7 @@ from _fel import *
 from _hyphyinterface import *
 from _mdr_variants import *
 from _mpi import *
+from _preprocessing import *
 from _rate_class_neb import *
 from _region_extract import *
 from _reporter import *
@@ -23,6 +24,7 @@ __all__ += _fel.__all__
 __all__ += _hyphyinterface.__all__
 __all__ += _mdr_variants.__all__
 __all__ += _mpi.__all__
+__all__ += _preprocessing.__all__
 __all__ += _rate_class_neb.__all__
 __all__ += _region_extract.__all__
 __all__ += _reporter.__all__
