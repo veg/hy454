ce268494fa2ef0ef72475aa11c5c239e4dc85c92
Lance Hepler <nlhepler@gmail.com>
2011-11-09 11:16:06 -0800

    upgrades to codonaligner, coveragegrapher, and graphing functionality

--- src/hy454/_graph.py
+++ src/hy454/_graph.py
@@ -11,23 +11,41 @@ __all__ = ['graph_coverage']
 
 _GAP = '-'
 
-def graph_coverage(refseq, alignment, filename=None, fmt='pdf'):
+def graph_coverage(alignment, filename=None, fmt='pdf'):
     if filename is None:
         fd, filename = mkstemp(); close(fd)
 
-    N = len(refseq)
-    frac = 1. / len(alignment)
+    M = len(alignment)
+    N = alignment.get_alignment_length()
+    frac = 1. / M
     height = np.zeros((N,), dtype=float)
-    sep = int(N / 5 / 25) * 25
+    xdiv = 10 ** int(np.log10(N)-0.3) or 1
+    xsep = int(float(N) / 5 / xdiv + 1) * xdiv
+    xticks = np.arange(xsep, N+xsep, xsep)
+    xticks[-1] = N-1
+    ydiv = 10 ** int(np.log10(M)-0.3) or 1
+    ysep = int(float(M) / 5 / ydiv + 1) * ydiv
+    yticks = np.arange(ysep, M+ysep, ysep)
+    yticks[-1] = M
 
     for i in xrange(N):
         height[i] = sum([frac for s in alignment if s.seq[i] != _GAP])
 
-    plt.bar(np.arange(N), height, width=1., color='black', edgecolor='black')
-    plt.xlabel('Reference Sequence Position')
-    plt.ylabel('Coverage')
-    plt.xticks(np.arange(0, N+1, sep))
-    plt.yticks(np.arange(0, 1.01, 0.2))
-    plt.savefig(filename, format=fmt)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+
+    ax1.bar(np.arange(N), height, width=1., color='black', edgecolor='black')
+    ax1.set_xlabel('Reference sequence position')
+    ax1.set_ylabel('Coverage')
+    # we don't need to set the xticks here because we do it for ax2 
+    # ax1.set_xticks(xticks)
+    ax1.set_yticks(np.arange(0.0, 1.1, 0.2))
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('# of sequences', rotation=270.)
+    ax2.set_xticks(xticks)
+    ax2.set_yticks(yticks)
+
+    fig.savefig(filename, format=fmt)
 
     return filename
