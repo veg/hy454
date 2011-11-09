8df14c3a3b9daf6f4a8994a7e59a392e91b9b4d7
Lance Hepler <nlhepler@gmail.com>
2011-11-08 23:43:46 -0800

    fix some more false assumptions in _graph.py

--- src/hy454/_graph.py
+++ src/hy454/_graph.py
@@ -18,14 +18,15 @@ def graph_coverage(refseq, alignment, filename=None, fmt='pdf'):
     N = len(refseq)
     frac = 1. / len(alignment)
     height = np.zeros((N,), dtype=int)
+    sep = int(N / 5 / 25) * 25
 
     for i in xrange(N):
-        height[i] = sum([frac for s in alignment if s[i] != _GAP])
+        height[i] = sum([frac for s in alignment if s.seq[i] != _GAP])
 
     plt.bar(np.arange(N), height, width=1., color='black', edgecolor='black')
     plt.xlabel('Reference Sequence Position')
-    plt.ylabel('Coverage (%)')
-    plt.xticks(np.arange(0, N+1, 20))
+    plt.ylabel('Coverage')
+    plt.xticks(np.arange(0, N+1, sep))
     plt.yticks(np.arange(0, 1.01, 0.2))
     plt.savefig(filename, format=fmt)
 
