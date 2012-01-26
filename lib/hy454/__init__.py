a2eac9d8441a3c15b50138329c358805e0782f9c
Lance Hepler <nlhepler@gmail.com>
2012-01-25 19:07:52 -0800

    fix imports

--- lib/hy454/__init__.py
+++ lib/hy454/__init__.py
@@ -5,6 +5,7 @@ from ._annotate import *
 from ._codonaligner import *
 from ._common import *
 from ._compensatory import *
+from ._divestimator import *
 from ._fel import *
 from ._graph import *
 from ._mdr_variants import *
@@ -14,7 +15,6 @@ from ._preprocessing import *
 from ._rate_class_neb import *
 from ._region_extract import *
 from ._reporter import *
-from ._sliding_window import *
 from ._variants import *
 
 
@@ -23,6 +23,7 @@ __all__ += _annotate.__all__
 __all__ += _codonaligner.__all__
 __all__ += _common.__all__
 __all__ += _compensatory.__all__
+__all__ += _divestimator.__all__
 __all__ += _fel.__all__
 __all__ += _graph.__all__
 __all__ += _mdr_variants.__all__
@@ -32,5 +33,4 @@ __all__ += _preprocessing.__all__
 __all__ += _rate_class_neb.__all__
 __all__ += _region_extract.__all__
 __all__ += _reporter.__all__
-__all__ += _sliding_window.__all__
 __all__ += _variants.__all__
