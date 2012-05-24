595b2dfbae6b771124c537d9d8a9293298daf3ab
Lance Hepler <nlhepler@gmail.com>
2012-05-24 14:35:04 -0700

    fix script installation, increment version again

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='hy454',
             'data/fonts/ttf/*.ttf',
             'data/scores/*.dat'
     ]},
-      data_files=[
+      scripts=[
             'scripts/codonaligner',
             'scripts/coveragegrapher',
             'scripts/seqlogo'
